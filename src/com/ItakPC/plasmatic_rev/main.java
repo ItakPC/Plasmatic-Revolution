d2fe75d4f7c25f7a52420856183f640c4ba1b0cf
ItakPC <buildingstar.com@gmail.com>
2014-10-26 13:52:38 +0100

    Main Class. Pretty much only basic rendering and exiting the app.

--- src/com/ItakPC/plasmatic_rev/main.java
+++ src/com/ItakPC/plasmatic_rev/main.java
@@ -1,7 +1,26 @@
 package com.ItakPC.plasmatic_rev;
 
-/**
- * Created by spravce on 10/26/2014.
- */
-public class main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void  main(String[] args) {
+
+        new Main();
+    }
+
+    public Main() {
+
+        /** Rendering */
+        JFrame frame = new JFrame();
+        //Title Of The Application.
+        frame.setTitle("Plasmatic Revolution");
+        //Start With Full Screen Window.
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
 }
